876416bbeb973d3089acbe9122ce5203c5341602
Bishwa-0 <bishwaranjanmishra2004@gmail.com>
2025-06-14 22:10:54 +0530

    Add files via upload

---
+++ User_Information_Table/js/app.js
@@ -0,0 +1,8 @@
+
+window.onload = () => {
+  const table = document.getElementById('userTable');
+  setTimeout(() => {
+    table.style.opacity = '1';
+    table.style.transform = 'translateY(0)';
+  }, 200);
+};
